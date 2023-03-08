1df492267e78994f53d95a9cbb7537a3fa70d0af
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-03-08 14:03:49 -0800

    lambda function

---
+++ index.js
@@ -0,0 +1,44 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+
+exports.handler = async (event) => {
+    let name = event.Records[0].s3.object.key;
+    let size = event.Records[0].s3.object.size;
+    let type = 'jpg';
+    let newImgObj = { name, size, type };
+    console.log('newImgObj', newImgObj);
+    
+    let images =[];
+    
+    let params = {
+        Bucket: 'afrey-bucket2',
+        Key:'images.json',
+    }
+    
+    try {
+        let data = await s3.getObject(params).promise();
+        console.log('data', data);
+        images = JSON.parse(data.Body.toString());
+        console.log('images',images);
+    }catch(e){
+        console.log(e.message);
+    }
+    
+    images.push(newImgObj);
+    params.Body = JSON.stringify(images);
+    
+    try {
+        await s3.putObject(params).promise();
+    }catch(e){
+        console.log(e.message);
+    }
+    
+    
+    // DONE implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(images),
+    };
+    return response;
+};
